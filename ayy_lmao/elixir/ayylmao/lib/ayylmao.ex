250efa1ba4b3c4b2ea102721f8a3d5b506c9159d
franklin <2730246+devsecfranklin@users.noreply.github.com>
2025-05-08 19:57:45 -0600

    upload older files

---
+++ ayy_lmao/elixir/ayylmao/lib/ayylmao.ex
@@ -0,0 +1,18 @@
+defmodule Ayylmao do
+  @moduledoc """
+  Documentation for Ayylmao.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ayylmao.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
